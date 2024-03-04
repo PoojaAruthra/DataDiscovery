b12f5ba265afa1264688dc5bb2372485d9204f96
Pooja A <pooja.a@siemens.com>
2024-03-04 11:10:17 +0530

    test

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,7 +73,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_server',
     'frontend',
-    # 'corsheaders'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -85,7 +85,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
