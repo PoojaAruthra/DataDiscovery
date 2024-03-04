4133e1c5646da7551b7d5ef6393116428c896757
Pooja A <pooja.a@siemens.com>
2024-03-04 17:02:12 +0530

    Testing

--- mysite/rest_server/oidc_authentication.py
+++ mysite/rest_server/oidc_authentication.py
@@ -1,25 +1,25 @@
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
 
-# class MyOIDCAB(OIDCAuthenticationBackend):
-#     def create_user(self, claims):
-#         user = super(MyOIDCAB, self).create_user(claims)
-#         user.email = claims.get('email', '')
-#         user.given_name = claims.get('given_name', '')
-#         user.family_name = claims.get('family_name', '')
-#         user.gid = claims.get('gid', '')
-#         user.org_code = claims.get('org_code', '')
-#         user.save()
+class MyOIDCAB(OIDCAuthenticationBackend):
+    def create_user(self, claims):
+        user = super(MyOIDCAB, self).create_user(claims)
+        user.email = claims.get('email', '')
+        user.given_name = claims.get('given_name', '')
+        user.family_name = claims.get('family_name', '')
+        user.gid = claims.get('gid', '')
+        user.org_code = claims.get('org_code', '')
+        user.save()
 
-#         return user
+        return user
 
-#     def update_user(self, user, claims):
-#         user.email = claims.get('email', '')
-#         user.given_name = claims.get('given_name', '')
-#         user.family_name = claims.get('family_name', '')
-#         user.gid = claims.get('gid', '').lower()
-#         user.org_code = claims.get('org_code', '')
-#         user.save()
+    def update_user(self, user, claims):
+        user.email = claims.get('email', '')
+        user.given_name = claims.get('given_name', '')
+        user.family_name = claims.get('family_name', '')
+        user.gid = claims.get('gid', '').lower()
+        user.org_code = claims.get('org_code', '')
+        user.save()
 
-#         return user
+        return user
         
