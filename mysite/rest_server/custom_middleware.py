1db0a51e3071edc88ca2e784ece03e5f55a290fa
Pooja A <pooja.a@siemens.com>
2024-03-27 13:34:58 +0530

    Testing

--- mysite/rest_server/custom_middleware.py
+++ mysite/rest_server/custom_middleware.py
@@ -15,32 +15,32 @@ class MyAuthMiddleware:
         #     jwt_token = request.session.get('jwt_token')
         #     print("session called")
         # else:
-        jwt_token = self.get_jwt_token(request)
+            # jwt_token = self.get_jwt_token(request)
             # request.session['jwt_token'] = jwt_token
-        if jwt_token:
+        # if jwt_token:
             # if 'user_details' in request.session:
             #     user_details = request.session.get('user_details')
             # else:
-            user_details = self.decode_jwt_token(jwt_token)
+                # user_details = self.decode_jwt_token(jwt_token)
                 # request.session['user_details'] = user_details
 
-            user= User.objects.get(email=user_details['email'])
-            print(user)
-            if user is None:
-                user = User.objects.create(email=user_details['email'], given_name=user_details['given_name'], family_name=user_details['family_name'], gid=user_details['gid'], org_code=user_details['org_code'])
-                user.save()
+            # user= User.objects.get(email=user_details['email'])
+            # print(user)
+            # if user is None:
+            #     user = User.objects.create(email=user_details['email'], given_name=user_details['given_name'], family_name=user_details['family_name'], gid=user_details['gid'], org_code=user_details['org_code'])
+            #     user.save()
             
-            # Set the authenticated user in the request
-            request.user = user
+            # # Set the authenticated user in the request
+            # request.user = user
         # else:
-        #     user= User.objects.get(email='pooja.a@siemens.com')
-        #     print(user)
-        #     if user is None:
-        #         user = User.objects.create(email='pooja.a@siemens.com',given_name='Pooja',family_name='A',gid='Z004K8AC',org_code='ADV D IN DASS PLT TE4 DAC2')
-        #         user.save()
-            
-        #     # Set the authenticated user in the request
-        #     request.user = user
+        user= User.objects.get(email='pooja.a@siemens.com')
+        print(user)
+        if user is None:
+            user = User.objects.create(email='pooja.a@siemens.com',given_name='Pooja',family_name='A',gid='Z004K8AC',org_code='ADV D IN DASS PLT TE4 DAC2')
+            user.save()
+        
+        # Set the authenticated user in the request
+        request.user = user
 
  
         # Continue with the request processing
