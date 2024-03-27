591159e5b406a809d897e4c963359413df3e6065
Pooja A <pooja.a@siemens.com>
2024-03-27 11:43:04 +0530

    Test

--- mysite/rest_server/custom_middleware.py
+++ mysite/rest_server/custom_middleware.py
@@ -11,39 +11,42 @@ class MyAuthMiddleware:
         self.get_response = get_response
  
     def __call__(self, request):
-        if 'jwt_token' in request.session:
-            jwt_token = request.session.get('jwt_token')
-            print("session called")
-        else:
-            jwt_token = self.get_jwt_token(request)
-            request.session['jwt_token'] = jwt_token
-        if jwt_token:
-            if 'user_details' in request.session:
-                user_details = request.session.get('user_details')
-            else:
-                user_details = self.decode_jwt_token(jwt_token)
-                request.session['user_details'] = user_details
+        # if 'jwt_token' in request.session:
+        #     jwt_token = request.session.get('jwt_token')
+        #     print("session called")
+        # else:
+        #     jwt_token = self.get_jwt_token(request)
+        #     request.session['jwt_token'] = jwt_token
+        # if jwt_token:
+        #     if 'user_details' in request.session:
+        #         user_details = request.session.get('user_details')
+        #     else:
+        #         user_details = self.decode_jwt_token(jwt_token)
+        #         request.session['user_details'] = user_details
 
-            user= User.objects.get(email=user_details['email'])
-            print(user)
-            if user is None:
-                user = User.objects.create_user(email=user_details['email'], given_name=user_details['given_name'], family_name=user_details['family_name'], gid=user_details['gid'], org_code=user_details['org_code'])
-                user.save()
-            
-            # Set the authenticated user in the request
-            request.user = user
- 
+        user= User.objects.get(email='pooja.a@siemens.com')
+        print(user)
+        if user is None:
+            user = User.objects.create(email='pooja.a@siemens.com',given_name='Pooja',family_name='A',gid='Z004K8AC',org_code='ADV D IN DASS PLT TE4 DAC2')
+            user.save()
+        
+        # Set the authenticated user in the request
+        request.user = user
+
         # Continue with the request processing
         response = self.get_response(request)
         return response
  
     def get_jwt_token(self, request):
         # url = 'auth/.me'   
-        url=settings.ProdURL   
-        response = requests.get(url+".auth/me")
+        
+        url=settings.ProdURL+'auth/.me'   
+        response = requests.get(url,timeout=5)
+        print("called",response.status_code)
         if response.status_code == 200:
             data = response.json()
             id_token = data[0].get('id_token')
+            print(id_token)
             return id_token
         else:
             return None
